742ceb9f43e5ed72c901aa3822be50e106fe2a7e
Joseph Costa <jhowboxer@hotmail.com>
2023-02-01 17:27:13 -0300

    fins de estudo

---
+++ config/seo.php
@@ -0,0 +1,7 @@
+<?php return array (
+  'meta_image' => 'meta.png',
+  'meta_keywords' => 'bet, bet365, betting, cricket, gambling, game, hyip, invest, ipl, live, lottery, online betting, online game, soccer, sports',
+  'meta_description' => 'LitoralBet - Uma plataforma de apostas online',
+  'social_title' => 'LitoralBet - Uma plataforma de apostas online',
+  'social_description' => 'LitoralBet - Uma plataforma de apostas online',
+);
